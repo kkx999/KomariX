4d16e96104b5d8ef53b8888e7856db5de372d1ff
KK <116494860+kkx999@users.noreply.github.com>
2026-09-18 17:10:26 +0800

    install: remove legacy Komari entrypoint

--- install-komari.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Legacy compatibility entrypoint. New installs should use install-komarix.sh.
-set -e
-exec bash <(curl -fsSL https://raw.githubusercontent.com/kkx999/KomariX/main/install-komarix.sh) "$@"
