87e471b4a653d8f2923ebeb0e08aab34a74c1a06
KK <116494860+kkx999@users.noreply.github.com>
2026-09-19 19:14:44 +0800

    feat: use Chinese default notification template

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -35,7 +35,7 @@ type Settings struct {
 	// 通知
 	NotificationEnabled        bool    `json:"notification_enabled" default:"true"` // 通知总开关
 	NotificationMethod         string  `json:"notification_method" default:"none"`
-	NotificationTemplate       string  `json:"notification_template" default:"{{emoji}}{{emoji}}{{emoji}}\nEvent: {{event}}\nClients: {{client}}\nMessage: {{message}}\nTime: {{time}}"`
+	NotificationTemplate       string  `json:"notification_template" default:"{{emoji}}{{emoji}}{{emoji}}\n事件：{{event}}\n节点：{{client}}\n消息：{{message}}\n时间：{{time}}"`
 	ExpireNotificationEnabled  bool    `json:"expire_notification_enabled" default:"true"` // 是否启用过期通知
 	ExpireNotificationLeadDays int     `json:"expire_notification_lead_days" default:"7"`  // 过期前多少天通知，默认7天
 	LoginNotification          bool    `json:"login_notification" default:"true"`          // 登录通知
